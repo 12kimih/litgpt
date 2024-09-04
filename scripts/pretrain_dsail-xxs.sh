7609e713623c977d9398558338b8650d54113acb
Ik-hwan Kim <12kimih@gmail.com>
2024-09-04 21:47:30 +0900

    add more dsail models

---
+++ scripts/pretrain_dsail-xxs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+__conda_setup="$($HOME/miniconda3/bin/conda shell.bash hook 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+conda activate litgpt
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 litgpt pretrain --config config_hub/pretrain/dsail-xxs.yaml
